c09d0d7b9b00bc965bfd3f7ad9ac20b43da0910c
Berna Yasar <bernayasar@hotmail.co.uk>
2023-01-23 09:40:01 +0000

    code

---
+++ src/containers/.js
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+
+const BakeryContainer = () => {
+
+    return(
+        <>
+        <h2>List of available cakes:</h2>
+        < Cake name={"Victoria Sponge"} ingredients={"eggs, butter, sugar, self-raising flour, baking powder, milk"} price={5} rating={5} />
+        {/* < Cake name={"Tea Loaf"} ingredients={} price={} rating={} />
+        < Cake name={"Carrot Cake"} ingredients={} price={} rating={} /> */}
+
+        </>
+    )
+}
+
+export default BakeryContainer;
